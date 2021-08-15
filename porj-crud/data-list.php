d5f6d4ffc176af11cfc1667790c060e848cf4af9
Portia Chen <portiachen81@gmail.com>
2021-08-15 16:48:31 +0800

    新增刪除警告及城市下拉選單

--- porj-crud/data-list.php
+++ porj-crud/data-list.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__. './partials/init.php';
+require __DIR__ . './partials/init.php';
 $title = "薬局";
 
 //如果沒有啟用 session, 就啟用
@@ -9,7 +9,7 @@ $title = "薬局";
 // }
 
 //建立一個$page幾頁的變數
-$perPage = 7; //固定一頁最多有幾筆資訊
+$perPage = 10; //固定一頁最多有幾筆資訊
 
 //用戶決定查看幾頁，預設為 1
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
@@ -21,38 +21,75 @@ $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 $totalRows = $PDO->query("SELECT count(1) FROM stores_list")->fetch(PDO::FETCH_NUM)[0];
 // echo $totalRows; exit;  //到這邊還是正常的
 
-//總共有幾頁，才能產生分頁按鈕
+//總共有幾頁，才能產生"分頁資料"
 //總頁數 = 總比數 / 每頁幾筆
 $totalPages = ceil($totalRows / $perPage); // ceil 正數，無條件進位
 // echo "$totalRows, $totalPages"; exit; //查看狀態是否正確
 
 
 //SELECT * FROM stores_list ORDER BY sid DESC LIMIT 0, 7
-$sql = sprintf("SELECT * FROM stores_list ORDER BY sid DESC LIMIT %s, %s",
-        ($page-1)*$perPage, $perPage);
+$sql = sprintf(
+    "SELECT * FROM stores_list ORDER BY sid ASC LIMIT %s, %s",
+    ($page - 1) * $perPage,
+    $perPage
+);
 
 $rows = $PDO->query($sql)
-            ->fetchAll();
+    ->fetchAll();
 
 ?>
 <style>
-    button>a{
-        text-decoration:none;
-        color:white;
+    button>a {
+        text-decoration: none;
+        color: white;
     }
-    .pd-word{
+
+    .pd-word {
         margin: 0;
         padding: 20px 0;
     }
-    
-    
+
+    .large1 {}
 </style>
 <?php include __DIR__ . './partials/html-head.php'; ?>
 <?php include __DIR__ . './partials/navnar.php'; ?>
 <div class="container">
     <div class="row justify-content-center align-items-center">
         <h2 class="pd-word">藥局門市資訊</h2>
+    </div>
+    <!-- 搜尋城市 -->
+    <div class="row justify-content-center align-items-center">
 
+        <div class="row">
+
+            <?php
+            $q = isset($_GET['q']) ? htmlspecialchars($_GET['q']) : '';
+            ?>
+    
+            <form action="" method="get">
+                <select name="q">
+                    <option value="">選擇一個地區:</option>
+                    <option value="臺北市">臺北市</option>
+                    <option value="桃園市">桃園市</option>
+                    <option value="新竹市">新竹市</option>
+                    <option value="臺中市">臺中市</option>
+                    <option value="屏東縣">屏東縣</option>
+                </select>
+                <input type="submit" value="搜尋">
+            </form>
+            <?php
+
+            if ($q != "") {
+                $sql = sprintf(
+                    "SELECT * FROM stores_list WHERE s_address rlike '$q' ORDER BY sid ASC LIMIT  0, 999",
+                    // 第0筆開始要印999筆
+                );
+            }
+            ?>
+
+        </div>
+    </div>
+    <div class="row justify-content-center align-items-center">
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -85,7 +122,7 @@ $rows = $PDO->query($sql)
                         <td>
                             <!-- 刪除 -->
                             <!-- del.php透過這個$_GET來取得編號的數字 -->
-                            <a href="del.php?id=<?php echo $row['sId'] ?>">
+                            <a href="del.php?id=<?php echo $row['sId'] ?>" onclick="javascript:return del();">
                                 <img src="./image/delete.png" alt="">
                             </a>
                         </td>
@@ -105,12 +142,46 @@ $rows = $PDO->query($sql)
         </table>
 
     </div>
-    <div>
-    
+    <div class="row justify-content-between">
+
+
         <button type="submit" class="btn btn-secondary">
-            <a href="add.php">新增藥局資訊</a>   
+            <a href="add.php">新增藥局</a>
         </button>
+
+        <!-- 跳頁碼 -->
+        <?php
+        if ($q == "") {
+        ?>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+
+                    <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+                        <li class="page-item <?= $i == $page ? 'active' : '' ?>">
+                            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                        <!--?開頭代表url是相同檔案 -->
+                    <?php endfor; ?>
+
+
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+        }
+        ?>
+
     </div>
+
 </div>
 
 <?php include __DIR__ . './partials/scripts.php'; ?>
@@ -118,42 +189,51 @@ $rows = $PDO->query($sql)
     //設定頁面的回應及提醒方式
     //先預設為通過，只要一個不通過，就是錯誤。
     //先建立一個變數為true,再看底下條件，其中一個條件成立那就把isPass = false (不通過)
-    function sendForm(){
+    function sendForm() {
         let isPass = true;
         document.form1.pName.nextElementSibling.style.display = 'none';
         document.form1.pAddress.nextElementSibling.style.display = 'none';
         document.form1.pPhone.nextElementSibling.style.display = 'none';
         document.form1.pTime.nextElementSibling.style.display = 'none';
 
-        if( ! document.form1.pName.value){
+        if (!document.form1.pName.value) {
             document.form1.pName.nextElementSibling.style.display = 'block';
             isPass = false;
         }
-        if( ! document.form1.pAddress.value){
+        if (!document.form1.pAddress.value) {
             document.form1.pAddress.nextElementSibling.style.display = 'block';
             isPass = false;
         }
-        if( ! document.form1.pPhone.value){
+        if (!document.form1.pPhone.value) {
             document.form1.pPhone.nextElementSibling.style.display = 'block';
             isPass = false;
         }
-        if( ! document.form1.pTime.value){
+        if (!document.form1.pTime.value) {
             document.form1.pTime.nextElementSibling.style.display = 'block';
             isPass = false;
         }
 
-        if (isPass){
+        if (isPass) {
             //fd=form data
             const fd = new FormData(document.form1);
 
-            fetche('add-list.php',{
-                method:'POST',
-                body:fd,
+            fetche('add-list.php', {
+                method: 'POST',
+                body: fd,
             })
             // .then(r=>.text())
             // .then(txt=>{
             //     console.log('result:',txt);
             // })
+        }
+    }
+
+    function del() {
+        var msg = "是否刪除此筆資料？";
+        if (confirm(msg)==true){
+        return true;
+        }else{
+        return false;
         }
     }
 </script>
--- porj-crud/partials/html-head.php
+++ porj-crud/partials/html-head.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./bootstrap4/css/bootstrap.css">
     <title><?= isset($title) ? $title. 'の' : '' ?>網站</title>
-    
 </head>
 
 <body>
